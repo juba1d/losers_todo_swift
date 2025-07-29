45c54234cacdda612dc86621aeeeeffe2469a288
jubaid <rusho.cs3@gmail.com>
2025-07-30 02:13:37 +0600

    Initial Commit

---
+++ Loser's Todo/Loser_s_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loser_s_TodoApp.swift
+//  Loser's Todo
+//
+//  Created by Jubaid Rusho on 30/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Loser_s_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Loser's TodoTests/Loser_s_TodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Loser_s_TodoTests.swift
+//  Loser's TodoTests
+//
+//  Created by Jubaid Rusho on 30/7/25.
+//
+
+import Testing
+@testable import Loser_s_Todo
+
+struct Loser_s_TodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
